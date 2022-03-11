e4c32161a010436ddc52a8dd58b00d49f3b590ce
Piper McCorkle <piper@lutris.engineering>
2022-03-10 18:32:15 -0600

    Force tap the element

--- UI Tests/UILaunchTests.swift
+++ UI Tests/UILaunchTests.swift
@@ -7,6 +7,17 @@
 
 import XCTest
 
+extension XCUIElement {
+    func forceTapElement() {
+         if self.isHittable {
+              self.tap()
+         } else {
+              let coordinate = self.coordinateWithNormalizedOffset(CGVectorMake(0.0, 0.0))
+              coordinate.tap()
+         }
+    }
+}
+
 class UILaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
@@ -49,7 +60,7 @@ class UILaunchTests: XCTestCase {
         }
         app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         let predicate = NSPredicate(format: "label CONTAINS 'Vehicle manufactured'")
-        app.tables.buttons.containing(predicate).element.tap()
+        app.tables.buttons.containing(predicate).element.forceTapElement()
         
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Log Item"
