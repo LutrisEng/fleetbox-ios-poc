722c6d5034f4ad12341e89f3d648ae69b8bc14d0
Piper McCorkle <piper@lutris.engineering>
2022-03-10 18:40:34 -0600

    Remove a test

--- UI Tests/UILaunchTests.swift
+++ UI Tests/UILaunchTests.swift
@@ -40,24 +40,4 @@ class UILaunchTests: XCTestCase {
         attachment.lifetime = .keepAlways
         add(attachment)
     }
-    
-    func testLogItem() throws {
-        let app = XCUIApplication()
-        app.launch()
-        if !app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists {
-            app.navigationBars["Vehicles"].buttons["Add Fixtures"].tap()
-        }
-        app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        let predicate = NSPredicate(format: "label CONTAINS 'Vehicle manufactured'")
-        let button = app.tables.buttons.containing(predicate).element
-        while !button.isHittable {
-            app.scrollViews.element(boundBy: 0).swipeUp()
-        }
-        button.tap()
-        
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Log Item"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
 }
