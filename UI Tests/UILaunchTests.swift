b20f88bbfb90bdf567d12917c6beaa7cc66da324
Piper McCorkle <piper@lutris.engineering>
2022-03-10 18:10:45 -0600

    Add UI tests

--- UI Tests/UILaunchTests.swift
+++ UI Tests/UILaunchTests.swift
@@ -16,16 +16,63 @@ class UILaunchTests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
+    
+    static let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+    
+    class func deleteApp() {
+        XCUIApplication().terminate()
+        
+         // Force delete the app from the springboard
+        let icon = springboard.icons["Fleetbox"]
+        if icon.exists {
+            icon.press(forDuration: 1.3)
+        
+            springboard.buttons["Remove App"].tap()
+            springboard.alerts.buttons["Delete App"].tap()
+            springboard.alerts.buttons["Delete"].tap()
+        }
+    }
+    
+    override func setUp() {
+        UILaunchTests.deleteApp()
+    }
+    
+    override func tearDown() {
+        UILaunchTests.deleteApp()
+    }
 
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+    
+    func testVehicle() throws {
+        let app = XCUIApplication()
+        app.launch()
+        app.navigationBars["Vehicles"].buttons["Add Fixtures"].tap()
+        app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Vehicle"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+    
+    func testLogItem() throws {
+        let app = XCUIApplication()
+        app.launch()
+        app.navigationBars["Vehicles"].buttons["Add Fixtures"].tap()
+        app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let predicate = NSPredicate(format: "label CONTAINS 'Vehicle manufactured'")
+        app.tables.buttons.containing(predicate).element.tap()
+        
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Log Item"
         attachment.lifetime = .keepAlways
         add(attachment)
     }
