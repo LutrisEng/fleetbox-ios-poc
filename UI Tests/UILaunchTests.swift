8d1fabb6a7528798e48f4aadb1f3de6614dee176
Piper McCorkle <piper@lutris.engineering>
2022-03-10 18:27:33 -0600

    Avoid having to reinstall the app in tests

--- UI Tests/UILaunchTests.swift
+++ UI Tests/UILaunchTests.swift
@@ -16,30 +16,6 @@ class UILaunchTests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
-    
-    static let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
-    
-    class func deleteApp() {
-        XCUIApplication().terminate()
-        
-         // Force delete the app from the springboard
-        let icon = springboard.icons["Fleetbox"]
-        if icon.exists {
-            icon.press(forDuration: 1.3)
-        
-            springboard.buttons["Remove App"].tap()
-            springboard.alerts.buttons["Delete App"].tap()
-            springboard.alerts.buttons["Delete"].tap()
-        }
-    }
-    
-    override func setUp() {
-        UILaunchTests.deleteApp()
-    }
-    
-    override func tearDown() {
-        UILaunchTests.deleteApp()
-    }
 
     func testLaunch() throws {
         let app = XCUIApplication()
@@ -54,7 +30,9 @@ class UILaunchTests: XCTestCase {
     func testVehicle() throws {
         let app = XCUIApplication()
         app.launch()
-        app.navigationBars["Vehicles"].buttons["Add Fixtures"].tap()
+        if !app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists {
+            app.navigationBars["Vehicles"].buttons["Add Fixtures"].tap()
+        }
         app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
         let attachment = XCTAttachment(screenshot: app.screenshot())
@@ -66,7 +44,9 @@ class UILaunchTests: XCTestCase {
     func testLogItem() throws {
         let app = XCUIApplication()
         app.launch()
-        app.navigationBars["Vehicles"].buttons["Add Fixtures"].tap()
+        if !app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists {
+            app.navigationBars["Vehicles"].buttons["Add Fixtures"].tap()
+        }
         app.tables/*@START_MENU_TOKEN@*/.buttons["The Mazda CX-5"]/*[[".cells[\"The Mazda CX-5\"].buttons[\"The Mazda CX-5\"]",".buttons[\"The Mazda CX-5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         let predicate = NSPredicate(format: "label CONTAINS 'Vehicle manufactured'")
         app.tables.buttons.containing(predicate).element.tap()
