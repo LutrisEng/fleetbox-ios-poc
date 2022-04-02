585d074b725af441e12242fc49db7d37f9a9395a
Piper McCorkle <piper@lutris.engineering>
2022-04-01 19:35:53 -0500

    Add embedded video to website

--- Documentation/src/pages/index.tsx
+++ Documentation/src/pages/index.tsx
@@ -32,9 +32,18 @@ export default function Home(): JSX.Element {
       title="Home"
       description={siteConfig.tagline}>
       <HomepageHeader />
-      {/* <main>
-        <HomepageFeatures />
-      </main> */}
+      <main className={styles.video}>
+        {/* <HomepageFeatures /> */}
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/9-wj8eMUEn0"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </main>
     </Layout>
   );
 }
